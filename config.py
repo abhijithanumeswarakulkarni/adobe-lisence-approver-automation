edba9fccb8905244172cb88073f25ed317436552
RAD_IT <rad_it@admins-Mac-mini-2.local>
2025-04-01 17:24:48 -0700

    Fixed some minor bugs

--- config.py
+++ config.py
@@ -2,6 +2,8 @@
 EMAIL = "roskiit@usc.edu"
 PASSWORD = "xnd8aej7khp*CTP5dpb"
 WUFOO_LOGIN_URL = "https://secure.wufoo.com/login/"
+WUFOO_SOURCE_PATH = "/Users/rad_it/Downloads/adobe-creative-cloud-access-spring-2025_entries.csv"
+WUFOO_DST_PATH = "/Users/rad_it/Desktop/adobe-lisence-approver-automation/adobe-creative-cloud-access-spring-2025_entries.csv"
 SEARCH_BAR_INPUT = "ADOBE CREATIVE CLOUD ACCESS SPRING 2025"
 ADOBE_URL ="https://www.adobe.com/"
 USC_ID = "roskiit"
--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 import config
 import time
 import pandas as pd
+import shutil
+import os
 
 # Configure Chrome to remain open
 chrome_options = webdriver.ChromeOptions()
@@ -20,22 +23,40 @@ browser.get(config.WUFOO_LOGIN_URL)
 
 wait = WebDriverWait(browser, 10)
 
-# Step 1: Enter Email
-print("Filling in Email...")
-email_input = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#emailInput")))
-email_input.send_keys(config.EMAIL)
+def login():
+    print("Trying to login.")
 
-# Step 2: Enter Password
-print("Filling in Password...")
-pwd_input = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#passwordLogin")))
-pwd_input.send_keys(config.PASSWORD)
+    # Step 1: Enter Email
+    print("Filling in Email...")
+    email_input = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#emailInput")))
+    email_input.send_keys(config.EMAIL)
 
-# Step 3: Click Submit
-print("Clicking Submit...")
-submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#saveForm")))
-submit.click()
+    # Step 2: Enter Password
+    print("Filling in Password...")
+    pwd_input = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#passwordLogin")))
+    pwd_input.send_keys(config.PASSWORD)
 
-print("Login Attempted. Checking for modal popup...")
+    # Step 3: Click Submit
+    print("Clicking Submit...")
+    submit = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#saveForm")))
+    submit.click()
+
+    time.sleep(5)
+
+login()
+
+while True:
+    try:
+        print("Checking if login was successful.")
+        error_text = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#loginBlock > p.error")))
+        print("Error loging in. Retrying.")
+        time.sleep(5)
+        login()
+    except Exception:
+        print("Login Successful. Proceeding to next step.")
+        break
+
+print("Checking for modal popup...")
 
 # Step 4: Check if modal exists and close it
 try:
@@ -43,17 +64,25 @@ try:
     print("Modal detected. Closing it...")
     close_button.click()
 except:
-    print("No modal found. Continuing...")
-
+    print("No modal found.")
 print("Proceeding with next steps.")
 
 # Step 5: Accept terms and conditions
-time.sleep(3)
-accept_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#onetrust-accept-btn-handler")))
-accept_button.click()
-time.sleep(1)
+try:
+    time.sleep(3)
+    accept_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#onetrust-accept-btn-handler")))
+    accept_button.click()
+    time.sleep(1)
+except Exception:
+    print("Terms pop up not present. Moving to next step.")
 
 # Step 6: Search for rule
+print("Checking if there is some content in the search bar.")
+try:
+    search_bar_close = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#search-query-delete")))
+    search_bar_close.click()
+except Exception:
+    print("No content in searchbar. Proceeding with next steps.")
 search_bar = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#search-bar")))
 search_bar.send_keys(config.SEARCH_BAR_INPUT)
 search_icon = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#search-query-icon")))
@@ -69,14 +98,13 @@ try:
 
     # Function to click and retry if checkbox is unchecked
     def click_with_retry(element):
-        # Attempt to click the checkbox
-        element.click()
-
-        # Retry clicking if the checkbox is not selected
-        time.sleep(1)  # Small delay to allow UI to update
-        if not element.is_selected():
+        while not element.is_selected():
             print("Element was not clicked, clicking again...")
-            element.click()  # Retry clicking
+            # Attempt to click the checkbox
+            element.click()
+
+            # Retry clicking if the checkbox is not selected
+            time.sleep(3)  # Small delay to allow UI to update
 
         # Confirm element is selected
         print(f"Element checked status: {element.is_selected()}")
@@ -96,11 +124,17 @@ try:
     # Click the second button (Excel)
     excel_button = download_options.find_element(By.CSS_SELECTOR, "button:nth-child(1)")
     excel_button.click()
-    print("Today's entries must be downloaded. Check the downloads folder.")
+    print("Today's entries must be downloaded.")
 
-except Exception as e:
-    print("No Entries Today or Issue Found!")
-    print("Error:", str(e))  # Debugging to check the real issue
+except Exception:
+    print("No Entries Today or Some Issue Occured!")
+
+print("Moving downloaded file to the root folder.")
+time.sleep(3)
+try:
+    shutil.move(config.WUFOO_SOURCE_PATH, config.WUFOO_DST_PATH)
+except Exception:
+    print("Error occured while moving file to root folder! Please retry.")
 
 # Step 8: Login into Adobe
 browser.get(config.ADOBE_URL)
@@ -160,9 +194,64 @@ except Exception as e:
 users_nav_link = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-testid='nav-item-users']")))
 users_nav_link.click()
 
-# Step 11: Read through new entries - Change the file name here for every run
+# Step 11: Read through new entries
 access_df = pd.read_csv("./adobe-creative-cloud-access-spring-2025_entries.csv")
-filtered_list = access_df[access_df["Please Select the Option that Applies to You"] != "Roski - Major" and access_df["Please Select the Option that Applies to You3"] != "Roski - MFA" and access_df["Please Select the Option that Applies to You4"] != "Roski - MA"].to_dict(orient="records")
-print(filtered_list)
+access_df = access_df.drop_duplicates()
+access_df = access_df[access_df["Please Select the Option that Applies to You"] != "Roski - Major"]
+access_df = access_df[access_df["Please Select the Option that Applies to You.3"] != "Roski - MFA"]
+filtered_list = access_df[access_df["Please Select the Option that Applies to You.4"] != "Roski - MA"].to_dict(orient="records")
+
+# Step 12: Assign License.
+email_filter_input = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-testid='table-filters-search-field']")))
+select_all_cb = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[aria-label='Select All']")))
+email_list = []
+for obj in filtered_list:
+    email = obj["USC Email Address"]
+    print("Checking if the license is already assigned to the email - " + email)
+    email_filter_input.send_keys(email)
+    email_filter_input.send_keys(Keys.RETURN)
+    time.sleep(2)
+    if(not select_all_cb.is_enabled()):
+        print("Email not present, adding the email " + email + " to the list.")
+        email_list.append({"firstName": obj["Name"], "lastName": obj["Last"], "email": email})
+    else:
+        print("Licence already assigned to the email.")
+    clear_filter = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[aria-label='Clear search']")))
+    clear_filter.click()
+
+print("Assigning Licences...")
+for obj in email_list:
+    first_name = obj["firstName"]
+    last_name = obj["lastName"]
+    email = obj["email"]
+    add_user_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-testid='add-users-btn']")))
+    add_user_btn.click()
+    time.sleep(2)
+    email_input = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-testid='user-picker']")))
+    email_input.send_keys(email)
+    time.sleep(5)
+    email_input.send_keys(Keys.DOWN)
+    email_input.send_keys(Keys.ENTER)
+    first_name_input = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-testid='first-name-field']")))
+    first_name_input.send_keys(first_name)
+    last_name_input = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-testid='last-name-field']")))
+    last_name_input.send_keys(last_name)
+    all_groups = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[data-testid='assignment-modal-open-button']")))
+    all_groups[1].click()
+    time.sleep(2)
+    search_bar = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-testid='user-group-search']")))
+    search_bar.send_keys("Roski Minors & Non Roski Students")
+    search_bar.send_keys(Keys.ENTER)
+    time.sleep(5)
+    checkbox = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div[aria-label='Roski Minors & Non Roski Students'] input[aria-label='Select'][type='checkbox']")))
+    checkbox.click()
+    time.sleep(1)
+    apply_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "section[data-testid='user-group-assignment-modal'] button[data-testid='cta-button']")))
+    apply_button.click()
+    # save_button= wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-testid='cta-button']")))
+
 input("Press Enter to close the Program:")
+if os.path.exists(config.WUFOO_DST_PATH):
+    os.remove(config.WUFOO_DST_PATH)
+    print(f"File has been deleted")
 browser.quit()
